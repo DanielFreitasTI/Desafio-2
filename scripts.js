147d75371ec522f7189f13b50c0e520998cb9a9a
Daniel Freitas <danielfreitasadm@gmail.com>
2021-07-26 00:07:22 -0300

    new file:   clientes.html 	modified:   index.html 	modified:   scripts.js 	modified:   style.css

--- scripts.js
+++ scripts.js
@@ -1,49 +1,72 @@
+function pegarDadosForm() {
+
+    data = {
+
+        "nome": document.querySelector('#nome').value,
+        "email": document.querySelector('#email').value,
+        "telefone": document.querySelector('#telefone').value,
+        "cpf": document.querySelector('#cpf').value,
+        "endereco": document.querySelector('#endereco').value,
+        "numero": document.querySelector('#numero').value,
+        "complemento": document.querySelector('#complemento').value,
+        "bairro": document.querySelector('#bairro').value,
+        "cep": document.querySelector('#cep').value,
+        "cidade": document.querySelector('#cidade').value,
+        "uf": document.querySelector('#estado').value
+    }
+};
+
+function pegarDadosDb() {
+
+    temp = JSON.parse(localStorage.getItem("db_client"));
+}
+
+function salvarDadosDb() {
+
+    db_client = [];
+
+    if (typeof temp === 'undefined' || temp === null) {
+        db_client.push(data);
+        localStorage.setItem("db_client", JSON.stringify(db_client));
+
+    } else {
+        temp.push(data);
+        localStorage.setItem("db_client", JSON.stringify(temp));
+    }
+
+    alert('Cliente cadastrado!');
+    document.getElementById("formCliente").reset();
+}
+
 function cadastrar() {
 
-    
-    
-let nome = document.querySelector('#nome').value
-let email = document.querySelector('#email').value
-let telefone = document.querySelector('#telefone').value
-let cpf = document.querySelector('#cpf').value
-let endereco = document.querySelector('#endereco').value
-let numero = document.querySelector('#numero').value
-let complemento = document.querySelector('#complemento').value
-let bairro = document.querySelector('#bairro').value
-let cep = document.querySelector('#cep').value
-let cidade = document.querySelector('#cidade').value
-let uf = document.querySelector('#estado').value
-
-let data = {
-
-    nome,
-    email,
-    telefone,
-    cpf,
-    endereco,
-    numero,
-    complemento,
-    bairro,
-    cep,
-    cidade,
-    uf
+    pegarDadosForm();
+    pegarDadosDb();
+    salvarDadosDb();
 }
 
-let convertData = JSON.stringify(data);
+function popularTabela() {
 
+    pegarDadosDb();
 
-    if (localStorage.idAtual) {
-        localStorage.idAtual = Number(localStorage.idAtual) + 1;
-        localStorage.setItem(`cliente_id: ${localStorage.idAtual}`, convertData);
-    } else {
-        localStorage.idAtual = 1;
-        localStorage.setItem(`cliente_id: ${localStorage.idAtual}`, convertData);
-      }
-      alert('Cliente cadastrado!');
-      document.getElementById("formCliente").reset();
-
+    for (i = 0; temp.length - 1; i++) {
+        console.log(temp[i].nome)
+    };
 }
 
 
+//let convertData = JSON.stringify(data);
+//let jsonData = JSON.parse(convertData)
 
 
+// if (localStorage.idAtual) {
+//     localStorage.idAtual = Number(localStorage.idAtual) + 1;
+//     localStorage.setItem(`cliente_id: ${localStorage.idAtual}`, convertData);
+// } else {
+//     localStorage.idAtual = 1;
+//     localStorage.setItem(`cliente_id: ${localStorage.idAtual}`, convertData);
+// }
+// alert('Cliente cadastrado!');
+// document.getElementById("formCliente").reset();
+
+// }
